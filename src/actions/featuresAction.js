cdd4852aeb183ee6359e6a8a8fbfd3bfa98cbb4f
Juana Villalpando <juana.v14@gmail.com>
2020-02-18 15:40:04 -0800

    added redux dependencies, action.js file, reducers.js file, and started my action and reducter

---
+++ src/actions/featuresAction.js
@@ -0,0 +1,10 @@
+export function addFeature(){
+    return{ 
+        type: 'ADD_FEATURE'
+    };
+}
+export function removeFeature(){
+    return{ 
+        type: 'REMOVE_FEATURE'
+    };
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+//added to file
+import {Provider} from 'react-redux';
+import {createStore } from 'redux';
+
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import { featuresReducer } from './reducers/featuresReducer';
+
+//imported const from app.js
+const store = createStore(featuresReducer)
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>
+, rootElement);
